236f371b66360105de95eea5be3aef0b366bb868
Medusa <155561619+MedusaScript@users.noreply.github.com>
2024-07-30 05:58:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-{
-  "name": "nodejs",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "keywords": [],
-  "author": "",
-  "license": "ISC",
-  "dependencies": {
-    "@types/node": "^18.0.6",
-    "axios": "^1.7.2",
-    "discord.js": "^14.15.3",
-    "discord.js-selfbot-v13": "^3.2.1"
-  }
-}
+
